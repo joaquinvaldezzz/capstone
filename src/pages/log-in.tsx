b2228067c824ba5943bbf8bbe869192722bf0862
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-04-17 23:05:29 +0800

    feat: create "Log in" page

---
+++ src/pages/log-in.tsx
@@ -0,0 +1,168 @@
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import Image from 'next/image'
+import Link from 'next/link'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import Title from '~/components/Title'
+import type { SubmitHandler } from 'react-hook-form'
+
+const schema = z.object({
+  username: z.string().min(4, 'Your username must be at least 4 characters.'),
+  password: z.string().min(8, 'Your password must be at least 8 characters.'),
+})
+
+type InputValues = z.infer<typeof schema>
+
+/**
+ * Renders the log-in page.
+ *
+ * @returns The log-in page component.
+ */
+export default function LogIn(): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<InputValues>({
+    resolver: zodResolver(schema),
+  })
+  const [form, setForm] = useState<InputValues>({
+    username: '',
+    password: '',
+  })
+
+  /**
+   * Handles the form submission.
+   *
+   * @param data - The form data to be submitted.
+   * @returns A Promise that resolves when the submission is successful.
+   */
+  async function onSubmit(data: SubmitHandler<InputValues>): Promise<void> {
+    await fetch('http://localhost:5050/record/', {
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+      .finally(() => {
+        console.log('Done')
+      })
+  }
+
+  return (
+    <div>
+      <Title>Log in</Title>
+
+      <div className="grid min-h-screen grid-cols-1 items-center md:grid-cols-2">
+        <div className="flex h-full flex-col justify-between">
+          <header className="px-8 pt-8">
+            <div className="h-8"></div>
+          </header>
+
+          <div className="flex w-full flex-col gap-8 px-4 py-12 md:mx-auto md:max-w-90 lg:p-0">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-display-xs font-semibold text-primary md:text-display-sm">
+                  Log in
+                </h1>
+                <p className="text-tertiary">
+                  Log in to your account to access the Skyline Hospital and Medical Center services.
+                </p>
+              </div>
+            </div>
+
+            <form
+              className="flex flex-col gap-6"
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onSubmit={handleSubmit(onSubmit as unknown as SubmitHandler<InputValues>)}
+            >
+              <div className="flex flex-col gap-1.5">
+                <label className="text-sm font-medium text-secondary" htmlFor="username">
+                  Username
+                </label>
+                <input
+                  className="rounded-lg bg-primary-bg px-3.5 py-2.5 text-primary shadow-xs ring-1 ring-inset ring-primary-border transition placeholder:text-placeholder focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-border data-error:ring-error-primary"
+                  id="username"
+                  {...register('username')}
+                  data-error={errors.username != null}
+                  type="text"
+                  value={form.username}
+                  autoComplete="given-name"
+                  placeholder=""
+                  onChange={(event) => {
+                    setForm({ ...form, username: event.target.value })
+                  }}
+                />
+                {errors.username != null && (
+                  <p className="text-sm text-error-primary">{errors.username.message}</p>
+                )}
+              </div>
+
+              <div className="flex flex-col gap-1.5">
+                <label className="text-sm font-medium text-secondary" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="rounded-lg bg-primary-bg px-3.5 py-2.5 text-primary shadow-xs ring-1 ring-inset ring-primary-border transition placeholder:text-placeholder focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-border data-error:ring-error-primary"
+                  id="password"
+                  {...register('password')}
+                  data-error={errors.password != null}
+                  type="password"
+                  value={form.password}
+                  autoComplete="current-password"
+                  placeholder=""
+                  onChange={(event) => {
+                    setForm({ ...form, password: event.target.value })
+                  }}
+                />
+                {errors.password != null && (
+                  <p className="text-sm text-error-primary">{errors.password.message}</p>
+                )}
+              </div>
+
+              <button
+                className="rounded-lg border border-button-primary-border bg-button-primary-bg px-4 py-2.5 font-semibold text-button-primary-fg shadow-xs transition focus:outline-none focus:ring-4 focus:ring-brand-500/25 focus:ring-offset-light-900"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? 'Signing in...' : 'Sign in'}
+              </button>
+            </form>
+
+            <p className="text-center text-sm text-tertiary">
+              Don&apos;t have an account?{' '}
+              <Link className="font-semibold text-button-tertiary-color-fg" href="/sign-up">
+                Sign up
+              </Link>
+            </p>
+          </div>
+
+          <footer className="px-8 pb-8">
+            <p className="text-sm text-tertiary">
+              &copy; Skyline Hospital and Medical Center {new Date().getFullYear()}
+            </p>
+          </footer>
+        </div>
+
+        <div className="relative h-full">
+          <Image
+            className="object-cover"
+            src="/images/sign-up/milad-fakurian-eRbgsJ0Ec0o-unsplash.jpg"
+            alt="Log in banner"
+            fill
+            sizes="(min-width: 1024px) 50vw, 100vw"
+            priority
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
