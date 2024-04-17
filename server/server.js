6cfee5ba6a95ae36bec6f86d0cc22b8111d8703e
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-04-17 13:32:48 +0800

    refactor: remove trailing slash

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: 'https://capstone-system-client.vercel.app/',
+    origin: 'https://capstone-system-client.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true,
   }),
