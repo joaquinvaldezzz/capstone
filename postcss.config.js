f971dc321f6377bd506740241d8ec3efc5d51e62
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-04-21 22:08:07 +0800

    build: temporarily disable purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
 const config = {
   plugins: {
-    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
-      content: ['./src/components/**/*.{ts,tsx}', './src/pages/**/*.{ts,tsx}'],
-      defaultExtractor: (content) => {
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || []
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
-        return broadMatches.concat(innerMatches)
-      },
-      keyframes: true,
-      variables: true,
-      safelist: ['html', 'body'],
-    },
+    // '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
+    //   content: ['./src/components/**/*.{ts,tsx}', './src/pages/**/*.{ts,tsx}'],
+    //   defaultExtractor: (content) => {
+    //     const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || []
+    //     const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
+    //     return broadMatches.concat(innerMatches)
+    //   },
+    //   keyframes: true,
+    //   variables: true,
+    //   safelist: ['html', 'body'],
+    // },
     autoprefixer: {},
     'postcss-import': {},
     'postcss-sort-media-queries': {},
