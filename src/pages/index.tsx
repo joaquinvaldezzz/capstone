f6514e6efb172ac589b776d148343565c42df001
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-05-24 22:25:26 +0800

    refactor: update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,127 +1,128 @@
-import { useState } from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { zodResolver } from '@hookform/resolvers/zod'
 import axios from 'axios'
-import type { SubmitHandler } from 'react-hook-form'
-import { useForm } from 'react-hook-form'
+import { useForm, type SubmitHandler } from 'react-hook-form'
 import { z } from 'zod'
 
-import { Button } from '~/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '~/components/ui/form'
 import { Input } from '~/components/ui/input'
-import { Label } from '~/components/ui/label'
 import Title from '~/components/Title'
 
-const schema = z.object({
-  username: z.string().min(4, 'Your username must be at least 4 characters.'),
-  password: z.string().min(8, 'Your password must be at least 8 characters.'),
+const formSchema = z.object({
+  username: z.string().min(4, 'Please enter your username.'),
+  password: z.string().min(8, 'Password must be at least 8 characters.'),
 })
+type FormValues = z.infer<typeof formSchema>
 
-type InputValues = z.infer<typeof schema>
-
-export default function Home(): JSX.Element {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { isSubmitting, errors },
-  } = useForm<InputValues>({
-    resolver: zodResolver(schema),
-  })
-  const [form, setForm] = useState<InputValues>({
-    username: '',
-    password: '',
+export default function SignIn(): JSX.Element {
+  const router = useRouter()
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+    resolver: zodResolver(formSchema),
   })
 
-  async function onSubmit(data: SubmitHandler<InputValues>): Promise<void> {
+  async function onSubmit(data: SubmitHandler<FormValues>): Promise<void> {
     try {
-      const response = await axios.post('http://localhost:5050/record/log-in', data)
+      const request = await axios.post('/api/accounts/log-in', data)
 
-      if (response.status === 200) {
-        setForm({
-          username: '',
-          password: '',
-        })
-        reset()
-
-        console.log('Logged in')
+      if (request.status === 200) {
+        await router.push('/dashboard/admin')
       }
-    } catch (error) {
-      console.error('Invalid password')
-    }
+    } catch (error) {}
   }
 
   return (
-    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-      <Title>Log in</Title>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <Title>Sign in</Title>
 
-      <div className="hidden lg:block lg:bg-muted">
-        <Image
-          className="size-full object-cover"
-          src="/images/sign-up/milad-fakurian-eRbgsJ0Ec0o-unsplash.jpg"
-          width="1920"
-          height="1080"
-          alt=""
-          sizes="(max-width: 640px) 640px, (max-width: 768px) 768px, (max-width: 1024px) 1024px, 1920px"
-          priority
-        />
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mx-auto size-10"></div>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
       </div>
 
-      <div className="flex items-center justify-center px-8 py-12 md:px-0">
-        <div className="mx-auto flex w-full max-w-96 flex-col gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-3xl font-bold">Welcome back!</h1>
-            <p className="text-balance text-muted-foreground">Please enter your details.</p>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form
+          className="space-y-6"
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onSubmit={form.handleSubmit(onSubmit as unknown as SubmitHandler<FormValues>)}
+        >
+          <Form {...form}>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      data-error={form.formState.errors.username != null}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center justify-between">
+                    <FormLabel>Password</FormLabel>
+                    <Link
+                      className="text-sm font-semibold text-indigo-600 hover:text-indigo-500"
+                      href="/forgot-password"
+                    >
+                      Forgot password?
+                    </Link>
+                  </div>
+
+                  <FormControl>
+                    <Input
+                      type="password"
+                      data-error={form.formState.errors.password != null}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </Form>
+
+          <div>
+            <button
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              type="submit"
+              disabled={form.formState.isSubmitting}
+            >
+              Sign in
+            </button>
           </div>
+        </form>
 
-          <form
-            className="flex flex-col gap-4"
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onSubmit={handleSubmit(onSubmit as unknown as SubmitHandler<InputValues>)}
-          >
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="username">Username</Label>
-              <Input
-                id="username"
-                {...register('username')}
-                data-error={errors.username != null}
-                type="text"
-                value={form.username}
-                placeholder=""
-                onChange={(event) => {
-                  setForm({ ...form, username: event.target.value })
-                }}
-              />
-              <p className="min-h-5 text-sm text-destructive">{errors.username?.message}</p>
-            </div>
-
-            <div className="relative flex flex-col gap-2">
-              <div className="flex items-center justify-between">
-                <Label htmlFor="password">Password</Label>
-                <Link className="inline-block text-sm underline" href="/forgot-password">
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input
-                className="pr-10"
-                id="password"
-                {...register('password')}
-                data-error={errors.password != null}
-                type="password"
-                value={form.password}
-                placeholder=""
-                onChange={(event) => {
-                  setForm({ ...form, password: event.target.value })
-                }}
-              />
-              <p className="min-h-5 text-sm text-destructive">{errors.password?.message}</p>
-            </div>
-
-            <Button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? 'Signing in...' : 'Sign in'}
-            </Button>
-          </form>
-        </div>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{' '}
+          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            Contact us to sign up
+          </a>
+        </p>
       </div>
     </div>
   )
