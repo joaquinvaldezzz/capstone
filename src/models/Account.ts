6ad2d382c0b5112bf22b5935e2363c6177be1da1
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-05-25 01:26:41 +0800

    chore: make some fields optional

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -4,11 +4,11 @@ export interface Account {
   _id: mongoose.Schema.Types.ObjectId
   first_name: string
   last_name: string
-  username: string
-  password: string
+  username?: string
+  password?: string
   role: 'admin' | 'doctor' | 'patient'
-  date_created: Date
-  date_updated: Date
+  date_created?: Date
+  date_updated?: Date
 }
 
 const accountSchema = new mongoose.Schema<Account>({
@@ -20,11 +20,11 @@ const accountSchema = new mongoose.Schema<Account>({
   },
   first_name: {
     type: String,
-    required: [true, 'Please enter your first name.'],
+    required: true,
   },
   last_name: {
     type: String,
-    required: [true, 'Please enter your last name.'],
+    required: true,
   },
   username: {
     type: String,
@@ -33,19 +33,19 @@ const accountSchema = new mongoose.Schema<Account>({
   },
   password: {
     type: String,
-    required: [true, 'Please enter your password.'],
+    required: false,
   },
   role: {
     type: String,
-    required: [true, 'Please indicate your role.'],
+    required: true,
   },
   date_created: {
     type: Date,
-    required: true,
+    required: false,
   },
   date_updated: {
     type: Date,
-    required: true,
+    required: false,
   },
 })
 
