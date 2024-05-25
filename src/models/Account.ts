09a891297ac77b8879ad7c6c2d2d01b711478322
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-05-25 13:49:03 +0800

    feat: add more fields

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -2,11 +2,28 @@ import mongoose from 'mongoose'
 
 export interface Account {
   _id: mongoose.Schema.Types.ObjectId
+
+  // Personal information
   first_name: string
   last_name: string
+  gender?: 'female' | 'male'
+  age?: number
+  birthdate?: Date
+  contact_number?: string
+  address?: {
+    street: string
+    city: string
+    province: string
+    zip_code: string | number
+    country: string
+  }
+
+  // Account information
   username?: string
   password?: string
-  role: 'admin' | 'doctor' | 'patient'
+  role?: 'admin' | 'doctor' | 'patient'
+
+  // Account creation and update information
   date_created?: Date
   date_updated?: Date
 }
@@ -18,6 +35,8 @@ const accountSchema = new mongoose.Schema<Account>({
     auto: true,
     unique: true,
   },
+
+  // Personal information
   first_name: {
     type: String,
     required: true,
@@ -26,6 +45,49 @@ const accountSchema = new mongoose.Schema<Account>({
     type: String,
     required: true,
   },
+  gender: {
+    type: String,
+    required: false,
+  },
+  age: {
+    type: Number,
+    required: false,
+  },
+  birthdate: {
+    type: Date,
+    required: false,
+  },
+  contact_number: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: {
+      street: {
+        type: String,
+        required: false,
+      },
+      city: {
+        type: String,
+        required: false,
+      },
+      zip_code: {
+        type: mongoose.Schema.Types.Mixed,
+        required: false,
+      },
+      province: {
+        type: String,
+        required: false,
+      },
+      country: {
+        type: String,
+        required: false,
+      },
+    },
+    required: false,
+  },
+
+  // Account information
   username: {
     type: String,
     required: false, // This is a unique field, but it's not required.
@@ -39,6 +101,8 @@ const accountSchema = new mongoose.Schema<Account>({
     type: String,
     required: true,
   },
+
+  // Account creation and update information
   date_created: {
     type: Date,
     required: false,
