61167e365daf2eecf4820c5f16bb6c5257c356b0
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-05-25 14:58:09 +0800

    style: sort attributes

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -43,8 +43,8 @@ const Toast = React.forwardRef<
 >(({ className, variant, ...props }, ref) => {
   return (
     <ToastPrimitives.Root
-      ref={ref}
       className={cn(toastVariants({ variant }), className)}
+      ref={ref}
       {...props}
     />
   )
