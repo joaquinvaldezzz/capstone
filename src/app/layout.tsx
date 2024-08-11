59b9091f69b56057c1c3907dcdf1fdb1776a29f6
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-08-11 14:52:09 +0800

    feat: use app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { type Metadata } from 'next'
+import localFont from 'next/font/local'
+
+import '../styles/main.css'
+
+export const metadata: Metadata = {
+  title: 'Capstone',
+}
+
+const inter = localFont({
+  src: [
+    {
+      path: '../../public/fonts/InterVariable.woff2',
+      weight: '100 900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/InterVariable-Italic.woff2',
+      weight: '100 900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-sans',
+})
+
+export default function Layout({ children }: { children: React.ReactNode }): JSX.Element {
+  return (
+    <html lang="en" className={inter.variable}>
+      <body className="min-w-80 antialiased">{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <p>I&apos;m under the water.</p>
+    </div>
+  )
+}
