ad8553ae18262e993bb4e3576099b00f378ee679
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-08-11 16:17:36 +0800

    feat: update input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,23 +1,30 @@
-import * as React from 'react'
-import { cn } from '~/lib'
+'use client'
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+import { forwardRef, type InputHTMLAttributes } from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-error:border-destructive data-error:ring-destructive',
-          className,
-        )}
-        type={type}
-        ref={ref}
-        {...props}
-      />
-    )
+import { cn } from '@/lib/utils'
+
+export const inputVariants = cva(
+  'flex w-full rounded-lg border border-gray-300 bg-white text-gray-900 shadow-xs placeholder:text-gray-500 focus:border-brand-300 focus:outline-none focus:ring-4 focus:ring-brand-500/25 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-50 aria-invalid:border-error-300 aria-invalid:ring-error-500/25 aria-invalid:placeholder:text-error-500',
+  {
+    variants: {
+      padding: {
+        sm: 'h-10 px-3 py-2',
+        md: 'h-11 px-3.5 py-2.5',
+      },
+    },
+    defaultVariants: {},
   },
 )
+
+export interface InputProps
+  extends InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(({ className, padding, ...props }, ref) => {
+  return <input className={cn(inputVariants({ padding }), className)} ref={ref} {...props} />
+})
 Input.displayName = 'Input'
 
 export { Input }
