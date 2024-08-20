1fb4046f7cfcd1dd23cefbe5e45c8b81092b07c9
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-08-20 23:21:25 +0800

    feat: create 'Log in' form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,41 @@
-import { type Metadata } from 'next'
+'use client'
+
 import Link from 'next/link'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
-export const metadata: Metadata = {
-  title: 'Log in',
-  description: 'Log in to your account',
-}
+const formSchema = z.object({
+  email_address: z.string().email({ message: 'Please enter your email address.' }),
+  password: z.string().min(8, { message: 'Your password must be at least 8 characters.' }),
+})
+
+type FormSchema = z.infer<typeof formSchema>
 
 export default function Page() {
+  const form = useForm<FormSchema>({
+    defaultValues: {
+      email_address: '',
+      password: '',
+    },
+    resolver: zodResolver(formSchema),
+  })
+
+  function onSubmit(values: FormSchema) {
+    console.log(values)
+  }
+
   return (
     <div className="min-h-svh py-12 lg:pt-24">
       <div className="mx-auto flex max-w-96 flex-col gap-y-8 px-4 md:px-0">
@@ -20,23 +47,51 @@ export default function Page() {
           <p className="mt-2 text-gray-600 lg:mt-3">Welcome back! Please enter your details.</p>
         </header>
 
-        <form className="flex flex-col gap-y-6">
-          <div></div>
-          <Button size="lg" type="submit">
-            Sign in
-          </Button>
-        </form>
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-y-6"
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onSubmit={form.handleSubmit(onSubmit as unknown as SubmitHandler<FormSchema>)}
+          >
+            <div className="flex flex-col gap-y-5">
+              <FormField
+                control={form.control}
+                name="email_address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input padding="md" type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input padding="md" type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button size="lg" type="submit">
+              Sign in
+            </Button>
+          </form>
+        </Form>
 
-        <footer className="text-center">
-          <p className="text-sm text-gray-600">
+        <footer>
+          <p className="text-center text-sm text-gray-600">
             Don&apos;t have an account?{' '}
             <Button hierarchy="link-color" asChild>
               <Link href="">Sign up</Link>
-            </Button>
-          </p>
-          <p className="mt-3">
-            <Button hierarchy="link-color" asChild>
-              <Link href="">Forgot password</Link>
             </Button>
           </p>
         </footer>
