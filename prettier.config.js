b01a8a64c3f4c69d555ece34e8731f6d0e220aa4
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-08-22 20:46:42 +0800

    chore: change config file type

---
+++ prettier.config.js
@@ -0,0 +1,32 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  plugins: [
+    '@prettier/plugin-xml',
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/registry/(.*)$',
+    '^@/styles/(.*)$',
+    '^@/app/(.*)$',
+    '',
+    '^[./]',
+    '',
+    '^@/public/(.*)$',
+  ],
+  tailwindFunctions: ['cva', 'cn'],
+}
