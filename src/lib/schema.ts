77ba7168632789656253de18c2b2dc611dc8e165
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-08-25 22:09:52 +0800

    feat: create schema for users table

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,9 +1,14 @@
+import { type InferInsertModel, type InferSelectModel } from 'drizzle-orm'
 import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
   email: text('email').notNull(),
-  image: text('image').notNull(),
+  password: text('password').notNull(),
+  role: text('role'),
   created_at: timestamp('created_at').defaultNow().notNull(),
 })
+
+export type User = InferSelectModel<typeof usersTable>
+export type NewUser = InferInsertModel<typeof usersTable>
