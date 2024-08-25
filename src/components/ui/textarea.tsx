d0e437c38d5f639cd9725691714eaa4e2be01cf9
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-08-25 21:57:00 +0800

    feat: use base variants from input component

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react'
-import { cn } from '~/lib'
+import { forwardRef, type TextareaHTMLAttributes } from 'react'
 
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+import { cn } from '@/lib/utils'
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+import { inputVariants } from './input'
+
+export interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
-        className={cn(
-          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className,
-        )}
+        className={cn(inputVariants(), 'min-h-32 px-3.5 py-3', className)}
         ref={ref}
         {...props}
       />
     )
   },
 )
-Textarea.displayName = 'Textarea'
 
-export { Textarea }
+Textarea.displayName = 'Textarea'
