83b0f4ad9ee0fcd75e0423b56212b82c0f26a3e0
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-02 17:29:43 +0800

    docs: create jsdoc for login action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -10,10 +10,16 @@ interface PreviousState {
   message: string
 }
 
-export async function login(
-  _previousState: PreviousState,
-  formData: FormData,
-): Promise<{ message: string }> {
+interface Message extends PreviousState {}
+
+/**
+ * Logs in a user with the provided form data.
+ *
+ * @param _previousState - The previous state of the application.
+ * @param formData - The form data containing the email and password.
+ * @returns A promise that resolves to a message indicating the result of the login operation.
+ */
+export async function login(_previousState: PreviousState, formData: FormData): Promise<Message> {
   const formValues = Object.fromEntries(formData)
   const parsedData = loginSchema.safeParse(formValues)
 
