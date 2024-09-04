dd9180d447b36fc4f309a799ca63ef7f67542a31
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-04 18:03:25 +0800

    feat: create getAllUsers

--- src/lib/dal.ts
+++ src/lib/dal.ts
@@ -5,6 +5,7 @@ import { eq } from 'drizzle-orm'
 
 import { db } from './db'
 import { users, type User } from './db-schema'
+import { type SignUpFormSchema } from './form-schema'
 import { verifySession } from './session'
 
 /**
@@ -20,11 +21,25 @@ export const getCurrentUser = cache(async (): Promise<User | null> => {
 
   try {
     const data = await db.select().from(users).where(eq(users.id, session.userId))
-    const user = data[0]
-
-    return user
+    return data[0]
   } catch (error) {
-    console.log('Failed to fetch user')
+    console.error('Failed to fetch user')
+    return null
+  }
+})
+
+/**
+ * Retrieves all users from the database.
+ *
+ * @returns A promise that resolves to an array of User objects if successful, or null if an error
+ *   occurs.
+ */
+export const getAllUsers = cache(async (role: SignUpFormSchema['role']): Promise<User[] | null> => {
+  try {
+    const data = await db.select().from(users).where(eq(users.role, role))
+    return data
+  } catch (error) {
+    console.error('Failed to fetch users')
     return null
   }
 })
