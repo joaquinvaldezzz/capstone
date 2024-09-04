ddc8e4fc7f4dc95536057be0b6193d20d20c357d
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-04 15:09:55 +0800

    feat: modify matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -38,5 +38,15 @@ export default async function middleware(request: NextRequest) {
 }
 
 export const config: MiddlewareConfig = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     *
+     * - Api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 }
