7a7ca6cb9b2aebab6c8f203ec5aa334d63357fb0
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-04 14:45:19 +0800

    feat: update middleware
    - docs: update comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,33 +10,30 @@ import { decrypt } from '@/lib/session'
  * @returns A NextResponse object representing the response to be sent.
  */
 export default async function middleware(request: NextRequest) {
-  // 1. Specify protected and public routes
+  // Specify protected and public routes
   const protectedRoutes = ['/dashboard']
   const publicRoutes = ['/']
 
-  // 2. Check if the current route is protected or public
+  // Check if the current route is protected or public
   const currentPath = request.nextUrl.pathname
   const isProtectedRoute = protectedRoutes.includes(currentPath)
   const isPublicRoute = publicRoutes.includes(currentPath)
 
-  // 3. Decrypt the session from the cookie
+  // Decrypt the session from the cookie
   const cookie = cookies().get('session')?.value
   const session = await decrypt(cookie)
 
-  // 4. Redirect if the route is protected and the user is not authenticated
+  // Redirect to the login page if the route is protected and the user is not authenticated
   if (isProtectedRoute && session?.userId == null) {
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
 
-  // 5. Redirect if the route is public and the user is authenticated
-  if (
-    isPublicRoute &&
-    session?.userId != null &&
-    !request.nextUrl.pathname.startsWith('/dashboard')
-  ) {
+  // Redirect to the dashboard if the route is public and the user is authenticated
+  if (isPublicRoute && session?.userId != null) {
     return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
   }
 
+  // Otherwise, continue to the next middleware
   return NextResponse.next()
 }
 
