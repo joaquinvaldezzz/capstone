14f0f307e60ce0f35cbaf43e4176e938b5af3a42
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-04 14:26:37 +0800

    feat: remove dark mode styles

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -38,14 +38,14 @@ const DialogContent = forwardRef<
     <DialogOverlay />
     <DialogPrimitive.Content
       className={cn(
-        'fixed left-1/2 top-1/2 z-50 max-h-[calc(100%-2rem)] w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded-xl bg-white shadow-xl duration-200 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-open:slide-in-from-left-1/2 data-open:slide-in-from-top-[48%] data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 data-closed:slide-out-to-left-1/2 data-closed:slide-out-to-top-[48%] sm:rounded-lg lg:w-full lg:max-w-screen-sm dark:border-gray-800 dark:bg-gray-950',
+        'fixed left-1/2 top-1/2 z-50 max-h-[calc(100%-2rem)] w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded-xl bg-white shadow-xl duration-200 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-open:slide-in-from-left-1/2 data-open:slide-in-from-top-[48%] data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 data-closed:slide-out-to-left-1/2 data-closed:slide-out-to-top-[48%] sm:rounded-lg lg:w-full lg:max-w-screen-sm',
         className,
       )}
       ref={ref}
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none data-open:bg-gray-100 data-open:text-gray-500 dark:ring-offset-gray-950 dark:focus:ring-gray-300 dark:data-open:bg-gray-800 dark:data-open:text-gray-400">
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none data-open:bg-gray-100 data-open:text-gray-500">
         <span className="sr-only">Close</span>
         <X className="size-4" />
       </DialogPrimitive.Close>
