5a91a1695223d2108088b0f1696566f7cf393e3c
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-11 00:00:52 +0800

    feat: modify file styles

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 export const inputVariants = cva(
-  'flex w-full rounded-lg bg-white text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-500 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-50 aria-invalid:ring-error-500 aria-invalid:placeholder:text-error-500',
+  'flex w-full rounded-lg bg-white text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 file:mr-2 file:border-0 file:bg-transparent file:p-0 file:font-medium placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-brand-500 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-50 aria-invalid:ring-error-500 aria-invalid:placeholder:text-error-500',
   {
     variants: {
       padding: {
