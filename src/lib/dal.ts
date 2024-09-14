3d1414e1f26ab14f2e7e7724864bd6f546f24cc7
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-15 02:17:07 +0800

    feat: create getPatientResults action

--- src/lib/dal.ts
+++ src/lib/dal.ts
@@ -24,7 +24,7 @@ export const getCurrentUser = cache(async (): Promise<User | null> => {
   if (session == null) return null
 
   try {
-    const data = await db.select().from(users).where(eq(users.id, session.userId))
+    const data = await db.select().from(users).where(eq(users.user_id, session.userId))
     return data[0]
   } catch (error) {
     console.error('Failed to fetch user')
@@ -59,7 +59,7 @@ export const getUsers = cache(async (role: SignUpFormSchema['role']): Promise<Us
  */
 export const getUserById = cache(async (id: number): Promise<User | null> => {
   try {
-    const data = await db.select().from(users).where(eq(users.id, id))
+    const data = await db.select().from(users).where(eq(users.user_id, id))
     return data[0]
   } catch (error) {
     console.error('Failed to fetch user')
@@ -80,6 +80,24 @@ export const getUserById = cache(async (id: number): Promise<User | null> => {
 export const getAllPatientResults = cache(async (): Promise<Result[] | null> => {
   try {
     const data = await db.select().from(results)
+    return data
+  } catch (error) {
+    console.error('Failed to fetch patient results')
+    return null
+  }
+})
+
+/**
+ * Retrieves the results for a specific patient from the database.
+ *
+ * @param patientId - The unique identifier of the patient whose results are to be fetched.
+ * @returns A promise that resolves to an array of `Result` objects if the query is successful, or
+ *   `null` if an error occurs.
+ * @throws Will log an error message to the console if the database query fails.
+ */
+export const getPatientResults = cache(async (patientId: number): Promise<Result[] | null> => {
+  try {
+    const data = await db.select().from(results).where(eq(results.user_id, patientId))
     return data
   } catch (error) {
     console.error('Failed to fetch patient results')
