be20cea85137f2d0b727ef59867bb1bc984caf78
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-14 22:50:49 +0800

    feat: create LogOutButton component

---
+++ src/components/log-out-button.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { LogOut } from 'lucide-react'
+
+import { logout } from '@/lib/actions'
+import { Button } from '@/components/ui/button'
+
+export function LogOutButton() {
+  return (
+    <Button
+      type="button"
+      hierarchy="tertiary-gray"
+      icon="sm"
+      size="sm"
+      onClick={() => {
+        void logout()
+      }}
+    >
+      <span className="sr-only">Log out</span>
+      <LogOut className="size-5" size={20} />
+    </Button>
+  )
+}
