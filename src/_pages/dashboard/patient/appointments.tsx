2b8c507f3e97e0675e74223991bb716bed610fb3
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-10-13 00:15:34 +0800

    chore: delete legacy pages

--- src/_pages/dashboard/patient/appointments.tsx
+++
@@ -1,9 +0,0 @@
-import PatientLayout from '~/components/layout/patient'
-
-export default function Appointments(): JSX.Element {
-  return (
-    <PatientLayout>
-      <h1 className="text-2xl font-semibold">Appointments</h1>
-    </PatientLayout>
-  )
-}
