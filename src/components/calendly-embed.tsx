abd683b8088da86dcc14fdd6a79cab712cb2e13a
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-10-16 23:22:38 +0800

    feat: create calendly embed

---
+++ src/components/calendly-embed.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export function CalendlyEmbed({ url }: { url: string }) {
+  useEffect(() => {
+    const head = document.querySelector('head')
+    const link = document.createElement('link')
+    const script = document.createElement('script')
+
+    link.rel = 'stylesheet'
+    link.href = 'https://assets.calendly.com/assets/external/widget.css'
+
+    script.setAttribute('src', 'https://assets.calendly.com/assets/external/widget.js')
+
+    if (head == null) return
+
+    head.appendChild(link)
+    head.appendChild(script)
+  }, [])
+
+  return <div className="calendly-inline-widget h-full" data-url={url}></div>
+}
