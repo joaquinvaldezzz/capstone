30c54c22b28a79fe65ccf2b5be2bd9ff36b589af
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-10-22 01:30:35 +0800

    chore: update import

--- src/hooks/use-mail.ts
+++ src/hooks/use-mail.ts
@@ -1,7 +1,7 @@
+import { mails } from '@/_app/(dashboards)/patient/data'
 import { atom, useAtom } from 'jotai'
 
 import { type Result } from '@/lib/dal'
-import { mails } from '@/app/(dashboards)/patient/data'
 
 interface Config {
   selected: Result['result_id'] | null
