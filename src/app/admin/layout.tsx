c65550d3828b49d3913122aaeebda089b7a980f1
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-10-22 05:40:03 +0800

    feat: create admin layout

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,41 @@
+import { type ReactNode } from 'react'
+import { type Metadata } from 'next'
+
+import { getCurrentUser } from '@/lib/dal'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/app-sidebar'
+import { type NavUserProps } from '@/components/nav-user'
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  const currentUser = await getCurrentUser()
+
+  if (currentUser == null) {
+    return null
+  }
+
+  const user: NavUserProps = {
+    user: {
+      name: currentUser.first_name.concat(' ', currentUser.last_name),
+      email: currentUser.email,
+      avatar: '',
+    },
+  }
+
+  return (
+    <SidebarProvider>
+      <AppSidebar user={user.user} />
+      <SidebarInset>
+        <header className="flex h-16 items-center">
+          <div className="flex flex-1 items-center justify-between gap-4 px-4">
+            <SidebarTrigger className="-ml-1" />
+          </div>
+        </header>
+        <main className="px-4 pb-4">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
