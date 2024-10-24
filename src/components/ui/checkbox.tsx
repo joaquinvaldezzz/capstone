c1db7e77d439f26f56e2e3b3fd816acf5232bf93
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-10-25 05:15:52 +0800

    feat: update checkbox component

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,44 +1,28 @@
 'use client'
 
-import { forwardRef, type ComponentPropsWithoutRef, type ElementRef, type SVGProps } from 'react'
-import { Indicator, Root } from '@radix-ui/react-checkbox'
+import { forwardRef, type ComponentPropsWithoutRef, type ElementRef } from 'react'
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import { Check } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
-interface CheckboxProps extends ComponentPropsWithoutRef<typeof Root> {
-  size?: 'sm' | 'md'
-}
+const Checkbox = forwardRef<
+  ElementRef<typeof CheckboxPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    className={cn(
+      'peer size-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      className,
+    )}
+    ref={ref}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
+      <Check className="size-4" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+))
+Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
-function CheckIcon({ ...props }: SVGProps<SVGSVGElement>) {
-  return (
-    <svg fill="none" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path
-        d="M10 3 4.5 8.5 2 6"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="1.667"
-      />
-    </svg>
-  )
-}
-
-export const Checkbox = forwardRef<ElementRef<typeof Root>, CheckboxProps>(
-  ({ className, size, ...props }, ref) => (
-    <Root
-      className={cn(
-        'aria-invalid:border-error-300 aria-invalid:ring-error-500/25 data-checked:border-brand-600 data-checked:bg-brand-600 data-checked:text-white peer shrink-0 cursor-pointer rounded border border-gray-300 bg-white focus:outline-none focus:ring-4 focus:ring-gray-400/15 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-300',
-        size === 'sm' ? 'size-4 rounded' : 'size-5 rounded-md',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    >
-      <Indicator className={cn('flex items-center justify-center text-current')}>
-        <CheckIcon className={cn(size === 'sm' ? 'size-3' : 'size-3.5')} />
-      </Indicator>
-    </Root>
-  ),
-)
-
-Checkbox.displayName = Root.displayName
+export { Checkbox }
