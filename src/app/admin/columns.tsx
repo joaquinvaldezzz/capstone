18e9178bc601bcc924d1ddd2daa27ba2f43ca469
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-10-26 16:50:42 +0800

    feat: use data table

--- src/app/admin/columns.tsx
+++ src/app/admin/columns.tsx
@@ -3,26 +3,37 @@
 import { type ColumnDef } from '@tanstack/react-table'
 import { format } from 'date-fns'
 
-import { type User } from '@/lib/db-schema'
+import { type CustomUser } from '@/lib/dal'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { DataTableColumnHeader } from '@/components/ui/data-table/data-table-column-header'
 
-export const columns: Array<ColumnDef<User>> = [
+export const columns: Array<ColumnDef<CustomUser>> = [
   {
     accessorKey: 'user_id',
-    header: 'User ID',
+    header: ({ column }) => <DataTableColumnHeader title="User ID" column={column} />,
     cell: (cell) => cell.row.original.user_id,
   },
   {
-    accessorKey: 'first_name',
-    header: 'Name',
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return <DataTableColumnHeader title="Name" column={column} />
+    },
     cell: (cell) => (
-      <span className="font-medium text-gray-900">
-        {cell.row.original.first_name} {cell.row.original.last_name}
-      </span>
+      <div className="flex items-center gap-2">
+        <Avatar>
+          <AvatarImage src="" />
+          <AvatarFallback>
+            {cell.row.original.first_name[0]}
+            {cell.row.original.last_name[0]}
+          </AvatarFallback>
+        </Avatar>
+        <span className="font-medium text-gray-900">{cell.row.original.name}</span>
+      </div>
     ),
   },
   {
     accessorKey: 'email',
-    header: 'Email address',
+    header: ({ column }) => <DataTableColumnHeader title="Email" column={column} />,
     cell: (cell) => (
       <div className="flex flex-1">
         <span className="truncate">{cell.row.original.email}</span>
@@ -31,12 +42,12 @@ export const columns: Array<ColumnDef<User>> = [
   },
   {
     accessorKey: 'role',
-    header: 'Role',
+    header: ({ column }) => <DataTableColumnHeader title="Role" column={column} />,
     cell: (cell) => <span className="capitalize">{cell.row.original.role}</span>,
   },
   {
     accessorKey: 'created_at',
-    header: 'Date created',
+    header: ({ column }) => <DataTableColumnHeader title="Date created" column={column} />,
     cell: (cell) => format(cell.row.original.creation_date, 'MMMM dd, yyyy h:mm a'),
   },
 ]
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -9,9 +9,9 @@ import {
   getTotalNumberOfPatients,
 } from '@/lib/dal'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { DataTable } from '@/components/ui/data-table'
 
 import { columns } from './columns'
-import { DataTable } from './data-table'
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -39,54 +39,53 @@ export default async function Page() {
 
   return (
     <Fragment>
-      <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-      <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total patients</CardTitle>
-            <Users className="size-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{totalNumberOfPatients[0].count}</div>
-            <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-          </CardContent>
-        </Card>
+      <section className="space-y-4">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total patients</CardTitle>
+              <Users className="size-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{totalNumberOfPatients[0].count}</div>
+              <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+            </CardContent>
+          </Card>
 
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Infected patients</CardTitle>
-            <ShieldAlert className="size-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{totalNumberOfInfectedPatients[0].count}</div>
-            <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-          </CardContent>
-        </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Infected patients</CardTitle>
+              <ShieldAlert className="size-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{totalNumberOfInfectedPatients[0].count}</div>
+              <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+            </CardContent>
+          </Card>
 
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Healthy patients</CardTitle>
-            <Activity className="size-4 text-muted-foreground" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{totalNumberOfHealthyPatients[0].count}</div>
-            <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-          </CardContent>
-        </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Healthy patients</CardTitle>
+              <Activity className="size-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{totalNumberOfHealthyPatients[0].count}</div>
+              <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+            </CardContent>
+          </Card>
+        </div>
+      </section>
 
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Doctors</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
-            <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-          </CardContent>
-        </Card>
-      </div>
-
-      <h3 className="mt-8 text-2xl font-semibold tracking-tight">Recently created users</h3>
-      <DataTable columns={columns} data={recentlyCreatedUsers} />
+      <section className="mt-8 space-y-4">
+        <h3 className="text-2xl font-semibold tracking-tight">Recently created users</h3>
+        <DataTable
+          columns={columns}
+          data={recentlyCreatedUsers}
+          withFacetedFilters
+          withViewOptions
+        />
+      </section>
     </Fragment>
   )
 }
