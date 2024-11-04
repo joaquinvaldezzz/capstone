08f818935bffe959779177d081b911be2266b441
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-11-05 01:05:22 +0800

    feat: generate fake user information

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 import bcrypt from 'bcrypt'
 
 import { db } from './db'
-import { users as usersTable } from './db-schema'
+import { userInformation, users } from './db-schema'
 
 const roles = ['admin', 'doctor', 'patient']
 
@@ -15,11 +15,27 @@ const roles = ['admin', 'doctor', 'patient']
  * - `email`: A randomly generated email address in lowercase.
  * - `role`: A randomly selected role from the `roles` array.
  */
-export const users = Array.from({ length: 100 }, () => ({
+export const fakeUsers = Array.from({ length: 100 }, () => ({
   firstName: faker.person.firstName(),
   lastName: faker.person.lastName(),
   email: faker.internet.email().toLowerCase(),
   role: faker.helpers.arrayElement(roles),
+}))
+
+/**
+ * Generates an array of 100 fake user information objects. Each object contains the following
+ * properties:
+ *
+ * - `age`: A random integer between 18 and 100.
+ * - `birthDate`: A random past date.
+ * - `gender`: A random gender.
+ * - `address`: A random street address.
+ */
+export const fakeUserInformation = Array.from({ length: 100 }, () => ({
+  age: faker.number.int({ min: 18, max: 100 }),
+  birthDate: faker.date.past(),
+  gender: faker.person.sex(),
+  address: faker.location.streetAddress(),
 }))
 
 /**
@@ -31,15 +47,37 @@ export const users = Array.from({ length: 100 }, () => ({
 export async function seed() {
   const defaultHashedPassword = await bcrypt.hash('5678ghjk@@!!##', 10)
 
-  await db
-    .insert(usersTable)
+  /** Inserts a list of fake users into the database and returns their IDs. */
+  const newUser = await db
+    .insert(users)
     .values(
-      users.map((user) => ({
+      fakeUsers.map((user) => ({
         first_name: user.firstName,
         last_name: user.lastName,
         email: user.email,
         password: defaultHashedPassword,
         role: user.role,
+      })),
+    )
+    .returning({ id: users.user_id })
+    .execute()
+
+  /**
+   * Inserts user information into the database.
+   *
+   * This function takes an array of user information objects, maps them to a format suitable for
+   * the database, and inserts them into the `usersInformation` table. Each user information object
+   * includes the user's ID, age, birth date, gender, and address.
+   */
+  await db
+    .insert(userInformation)
+    .values(
+      fakeUserInformation.map((information, index) => ({
+        user_id: newUser[index].id,
+        age: information.age,
+        birth_date: information.birthDate,
+        gender: information.gender,
+        address: information.address,
       })),
     )
     .execute()
