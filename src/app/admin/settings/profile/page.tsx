f6b090539db8ce674c2f5d48352c785df7e59406
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-11-05 01:25:11 +0800

    feat: create "Profile" page

--- src/app/admin/settings/profile/page.tsx
+++ src/app/admin/settings/profile/page.tsx
@@ -1,4 +1,9 @@
 import { type Metadata } from 'next'
+import { sql } from 'drizzle-orm'
+
+import { getCurrentUser } from '@/lib/dal'
+import { db } from '@/lib/db'
+import { type UserInformation } from '@/lib/db-schema'
 
 import { ProfileForm } from './profile-form'
 
@@ -6,16 +11,38 @@ export const metadata: Metadata = {
   title: 'Profile',
 }
 
-export default function Page() {
+export default async function Page() {
+  const user = await getCurrentUser()
+
+  if (user == null) {
+    throw new Error('User not found')
+  }
+
+  const profileInformation = await db.execute(sql`
+    SELECT
+      *
+    FROM
+      "users"
+      RIGHT JOIN "user_information" ON "users"."user_id" = "user_information"."user_id"
+    WHERE
+      "users"."user_id" = ${user.user_id};
+    `)
+
+  if (profileInformation == null) {
+    throw new Error('Profile not found')
+  }
+
+  const userProfile = profileInformation.rows[0] as UserInformation
+
   return (
     <div className="flex flex-col gap-6">
       <div>
         <h3 className="text-2xl font-semibold tracking-tight">Profile</h3>
         <p className="text-sm text-muted-foreground">
-          Manage your account settings here including your email, password, and other preferences.
+          Manage your profile settings such as your age, date of birth, address, and gender.
         </p>
       </div>
-      <ProfileForm />
+      <ProfileForm data={userProfile} />
     </div>
   )
 }
--- src/app/admin/settings/profile/profile-form.tsx
+++ src/app/admin/settings/profile/profile-form.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-import { startTransition, useActionState, useRef, type FormEvent } from 'react'
+import { startTransition, useActionState, useEffect, useRef, type FormEvent } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { format } from 'date-fns'
 import { Loader2 } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 
-import { signUp } from '@/lib/actions'
+import { updateProfile } from '@/lib/actions'
+import { type UserInformation } from '@/lib/db-schema'
 import { updateProfileFormSchema, type UpdateProfileFormSchema } from '@/lib/form-schema'
+import { useToast } from '@/hooks/use-toast'
 import { Button } from '@/components/ui/button'
 import {
   Form,
@@ -26,19 +28,19 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 
-export function ProfileForm() {
+export function ProfileForm({ data }: { data: UserInformation }) {
   const formRef = useRef<HTMLFormElement>(null)
-  const [, formAction, isSubmitting] = useActionState(signUp, { message: '' })
-
+  const [formState, formAction, isSubmitting] = useActionState(updateProfile, { message: '' })
   const form = useForm<UpdateProfileFormSchema>({
     defaultValues: {
-      age: '',
-      birth_date: new Date(),
-      gender: '',
-      address: '',
+      age: data.age.toString(),
+      birth_date: data.birth_date,
+      address: data.address,
+      gender: data.gender,
     },
     resolver: zodResolver(updateProfileFormSchema),
   })
+  const { toast } = useToast()
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     // Prevent the default form submission behavior.
@@ -55,6 +57,23 @@ export function ProfileForm() {
     })(event)
   }
 
+  useEffect(() => {
+    if (formState.message.length > 0) {
+      if (formState.success ?? false) {
+        toast({
+          title: 'Yay!',
+          description: formState.message,
+        })
+      } else {
+        toast({
+          title: 'Oops!',
+          description: formState.message,
+          variant: 'destructive',
+        })
+      }
+    }
+  }, [formState, toast])
+
   return (
     <Form {...form}>
       <form
@@ -63,6 +82,7 @@ export function ProfileForm() {
         ref={formRef}
         onSubmit={handleSubmit}
       >
+        <input name="id" type="text" value={data.user_id} hidden readOnly />
         <div className="flex flex-col gap-8">
           <div className="grid grid-cols-2 gap-4">
             <FormField
@@ -88,7 +108,7 @@ export function ProfileForm() {
                     <Input
                       name={field.name}
                       type="date"
-                      value={field.value != null ? format(field.value, 'yyyy-mm-dd') : ''}
+                      value={field.value != null ? format(field.value, 'yyyy-MM-dd') : ''}
                       ref={field.ref}
                       onBlur={field.onBlur}
                       onChange={field.onChange}
@@ -120,7 +140,7 @@ export function ProfileForm() {
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Gender</FormLabel>
-                <Select name="role" defaultValue={field.value} onValueChange={field.onChange}>
+                <Select name="gender" defaultValue={field.value} onValueChange={field.onChange}>
                   <FormControl>
                     <SelectTrigger>
                       <SelectValue placeholder="Select your gender" />
