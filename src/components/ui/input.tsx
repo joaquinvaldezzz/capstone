1b4d6675aa704a751d3c1690561b2218992af74a
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-11-09 22:43:12 +0800

    chore: export component variants

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,16 @@
 import { forwardRef, type InputHTMLAttributes } from 'react'
+import { cva } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
+export const inputVariants = cva(
+  'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[disabled]:opacity-50 data-[focus-within]:outline-none data-[focus-within]:ring-2 data-[focus-within]:ring-ring data-[focus-within]:ring-offset-2',
+)
+
 const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      className={cn(
-        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
-      type={type}
-      ref={ref}
-      {...props}
-    />
-  )
+  return <input className={cn(inputVariants(), className)} type={type} ref={ref} {...props} />
 })
 Input.displayName = 'Input'
 
