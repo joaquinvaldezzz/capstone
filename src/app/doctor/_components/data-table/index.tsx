329a85792f5354c5e1abc354ebb3b75177faa346
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-11-12 06:06:50 +0800

    feat: create dynamic page for patient results

--- src/app/doctor/_components/data-table/index.tsx
+++ src/app/doctor/_components/data-table/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Fragment, useState, type JSX } from 'react'
+import Link from 'next/link'
 import {
   flexRender,
   getCoreRowModel,
@@ -132,6 +133,10 @@ export function DataTable<TData extends Result, TValue>({
                 <TableRow data-state={row.getIsSelected() && 'selected'} key={row.id}>
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id}>
+                      <Link
+                        className="absolute inset-0"
+                        href={`/doctor/results/${cell.row.original.result_id}`}
+                      />
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </TableCell>
                   ))}
---
+++ src/app/doctor/results/[id]/print-button.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { PrinterIcon } from '@heroicons/react/24/outline'
+
+import { Button } from '@/components/ui/button'
+
+export function PrintButton() {
+  return (
+    <Button
+      onClick={() => {
+        window.print()
+      }}
+    >
+      <PrinterIcon className="size-5" />
+      <span>Print</span>
+    </Button>
+  )
+}
