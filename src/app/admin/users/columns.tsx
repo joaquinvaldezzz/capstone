75111c6320c42cf25e706fb49ca7e672fc473870
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-11-11 22:47:59 +0800

    feat: display users' profile picture

--- src/app/admin/users/columns.tsx
+++ src/app/admin/users/columns.tsx
@@ -2,10 +2,20 @@
 
 import { type ColumnDef } from '@tanstack/react-table'
 import { format } from 'date-fns'
+import { Ellipsis } from 'lucide-react'
 
 import { type CustomUser } from '@/lib/dal'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
 import { DataTableColumnHeader } from '@/components/ui/data-table/data-table-column-header'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 export const columns: Array<ColumnDef<CustomUser>> = [
   {
@@ -21,7 +31,10 @@ export const columns: Array<ColumnDef<CustomUser>> = [
     cell: (cell) => (
       <div className="flex items-center gap-2">
         <Avatar>
-          <AvatarImage src="" />
+          <AvatarImage
+            src={`https://x5l8gkuguvp5hvw9.public.blob.vercel-storage.com/profile-pictures/${cell.row.original.profile_picture}`}
+            alt={cell.row.original.name}
+          />
           <AvatarFallback>
             {cell.row.original.first_name[0]}
             {cell.row.original.last_name[0]}
@@ -54,5 +67,28 @@ export const columns: Array<ColumnDef<CustomUser>> = [
     accessorKey: 'date_modified',
     header: ({ column }) => <DataTableColumnHeader title="Date modified" column={column} />,
     cell: (cell) => format(cell.row.original.date_modified, 'MMMM dd, yyyy h:mm a'),
+  },
+  {
+    id: 'actions',
+    cell: function Cell(row) {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="flex size-8 p-0 data-[state=open]:bg-muted" variant="ghost">
+              <Ellipsis className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-40" align="end">
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              Delete
+              <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
