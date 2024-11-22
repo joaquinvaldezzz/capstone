c9ab2dcd59bd54675edd2cc29a3a5cee44ca3c2d
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-11-22 21:37:45 +0800

    chore: remove commented code

--- src/app/patient/page.tsx
+++ src/app/patient/page.tsx
@@ -8,34 +8,7 @@ export const metadata: Metadata = {
 export default async function Page() {
   return (
     <Fragment>
-      {/* <div className="flex flex-1 flex-col">
-        <div className="flex items-start p-4">
-          <div className="flex items-start gap-4 text-sm">
-            <Avatar>
-              <AvatarImage alt={mail.name} />
-              <AvatarFallback>
-                {mail.name
-                  .split(' ')
-                  .map((chunk) => chunk[0])
-                  .join('')}
-              </AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <div className="font-semibold">{mail.name}</div>
-              <div className="line-clamp-1 text-xs">{mail.subject}</div>
-              <div className="line-clamp-1 text-xs">
-                <span className="font-medium">Reply-To:</span> {mail.email}
-              </div>
-            </div>
-          </div>
-          {mail.date && (
-            <div className="ml-auto text-xs text-muted-foreground">
-              {format(new Date(mail.date), 'PPpp')}
-            </div>
-          )}
-        </div>
-      </div> */}
-      <h2>Hello</h2>
+      <h2></h2>
     </Fragment>
   )
 }
